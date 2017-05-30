b5a94367a841f087d4affea973c16ff77dd97263
Robert Danitz <robert@advancedtelematic.com>
2017-05-30 18:11:57 +0200

    fix deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,8 @@ fi
 
 export DOCKER_TAG=$1
 export JOB_NAME="${JOB_NAME-campaigner}"
-export VAULT_ENDPOINT=${VAULT_ENDPOINT-$(echo $JOB_NAME | tr "-" "_")}
+export VAULT_SECRET=$(echo $JOB_NAME | tr "-" "_")
+export VAULT_ENDPOINT="http://secrets.prod01.internal.advancedtelematic.com:8200/v1/secret/${VAULT_SECRET}"
 export IMAGE_NAME="campaigner"
 export REGISTRY="advancedtelematic"
 export IMAGE_ARTIFACT=${REGISTRY}/${IMAGE_NAME}:${DOCKER_TAG}
@@ -17,7 +18,7 @@ export USE_CPU="0.5"
 export JAVA_OPTS="-Xmx800m"
 
 # Merge service environment variables with secrets from this vault endpoint.
-export CATALOG_ADDR="http://catalog.gw.prod01.internal.advancedtelematic.com"
+export MARATHON="http://marathon.prod01.internal.advancedtelematic.com:8080"
 
 cat deploy/service.json |
     envsubst |
