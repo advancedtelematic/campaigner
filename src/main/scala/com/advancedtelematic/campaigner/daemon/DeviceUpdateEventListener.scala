c5c40b96da6d293616a76057fc13dda3216dd48b
Jerry Trieu <jerry.trieu@here.com>
2019-03-12 16:58:18 +0100

    Handle DeviceUpdateCanceled event
    Signed-off-by: Jerry Trieu <jerry.trieu@here.com>

--- src/main/scala/com/advancedtelematic/campaigner/daemon/DeviceUpdateEventListener.scala
+++ src/main/scala/com/advancedtelematic/campaigner/daemon/DeviceUpdateEventListener.scala
@@ -5,6 +5,7 @@ import com.advancedtelematic.campaigner.data.DataType._
 import com.advancedtelematic.campaigner.db.{Campaigns, UpdateSupport}
 import com.advancedtelematic.campaigner.http.Errors
 import com.advancedtelematic.libats.data.DataType.{CampaignId => CampaignCorrelationId, MultiTargetUpdateId}
+import com.advancedtelematic.libats.messaging_datatype.DataType.UpdateId
 import com.advancedtelematic.libats.messaging_datatype.Messages.{
   DeviceUpdateEvent,
   DeviceUpdateCanceled,
@@ -23,9 +24,25 @@ class DeviceUpdateEventListener()(implicit db: Database, ec: ExecutionContext)
 
   def apply(event: DeviceUpdateEvent): Future[Unit] = {
     event match {
+      case msg: DeviceUpdateCanceled  => handleUpdateCanceled(msg)
       case msg: DeviceUpdateCompleted => handleUpdateCompleted(msg)
       case _ => Future.successful(())
     }
+  }
+
+  def handleUpdateCanceled(msg: DeviceUpdateCanceled): Future[Unit] = {
+      msg.correlationId match {
+        case CampaignCorrelationId(uuid) =>
+          campaigns.finishDevices(CampaignId(uuid), Seq(msg.deviceUuid), DeviceStatus.cancelled)
+        case MultiTargetUpdateId(uuid) =>
+          campaigns.findCampaignsByUpdate(UpdateId(uuid)).flatMap {
+            case cs if cs.isEmpty =>
+              _log.info(s"No campaign exists for ${msg.deviceUuid}")
+              Future.successful(())
+            case _ =>
+              campaigns.finishDevice(UpdateId(uuid), msg.deviceUuid, DeviceStatus.cancelled)
+          }
+      }
   }
 
   def handleUpdateCompleted(msg: DeviceUpdateCompleted): Future[Unit] = {
